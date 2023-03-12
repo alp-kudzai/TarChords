f52e79a92940d146c33665553253e6294a2ba0a8
Kudzai <alpha.kudzai@gmail.com>
2023-03-12 09:29:17 +0200

    Updates

---
+++ _app/immutable/chunks/1.0611a112.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.960f7941.js";export{t as component};
--- _app/immutable/chunks/1.496ac263.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c9b18d5c.js";export{t as component};
